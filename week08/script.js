ab81c8a903ef4d5055e33b2b3c5e6b23a90bc445
nyllhad <104041746+nyllhad@users.noreply.github.com>
2022-07-20 23:24:18 -0600

    Delete week08 directory

--- week08/script.js
+++
@@ -1,9 +0,0 @@
-function toggleNav() {
-             var updateElement = document.getElementById("list");
-          //toggle adds a class if it's not there or removes it if it is.
-          updateElement.classList.toggle("open");
-      }   
-
-  function barSwitch(x) {
-    x.classList.toggle("bar-switch");
-  }
